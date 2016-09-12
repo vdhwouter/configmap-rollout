60e40f5c013dc8aa229b1ed47a88dd26d4e397da
Michail Kargakis <mkargaki@redhat.com>
2016-09-12 17:39:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+### Config map rollout
+
+This is a proof-of-concept for https://github.com/kubernetes/kubernetes/pull/31701.
+
+Supposing you are running a local Kubernetes cluster via `hack/local-cluster-up.sh`, clone this repository under /tmp:
+```sh
+$ git clone https://github.com/kargakis/configmap-rollout /tmp/configmap-rollout
+$ cd /tmp/configmap-rollout
+```
+
+`observer.yaml` includes:
+1) The observer Deployment that is running [oc observe](https://github.com/openshift/origin/blob/master/images/observe/Dockerfile) inside a Pod (see https://github.com/kubernetes/kubernetes/issues/5164 for background) and uses a hostPort in order for the command to be able to contact the api server.
+2) The script that runs every time a config map is created or updated (trigger.sh) mounted in the Deployment as a ConfigMap.
+3) A kubeconfig that works ootb with `local-cluster-up.sh` mounted in the Deployment as a ConfigMap. If you need a different kubeconfig, delete the existing one and add yours:
+```sh
+$ kubectl delete cm kubeconfig
+configmap "kubeconfig" deleted
+$ kubectl create cm kubeconfig --from-file=$KUBECONFIG
+configmap "kubeconfig" created
+```
+
+Create the custom controller that will update Deployments on ConfigMap updates:
+```sh
+$ kubectl create -f observer.yaml
+configmap "kubeconfig" created
+configmap "trigger" created
+deployment "observer" created
+```
+
+
+### Example
+
+```
+$ kubectl create -f example/example.yaml
+configmap "alpine" created
+deployment "alpine" created
+```
+
+At this point, you should have the following pods and config maps in your namespace:
+```
+$ kubectl get po
+NAME                        READY     STATUS    RESTARTS   AGE
+alpine-928338976-59vhm      1/1       Running   0          17s
+observer-3223417557-xf4aj   1/1       Running   0          2m
+```
+```
+$ kubectl get cm
+NAME         DATA      AGE
+alpine       1         19s
+kubeconfig   1         2m
+trigger      1         2m
+```
+
+We are interested specifically in the alpine deployment and config map. You should be able to inspect our custom configuration in the alpine pod with the following command:
+```
+$ kubectl exec alpine-928338976-59vhm -i -t -- cat /etc/my-key
+Hello there!
+```
+
+Currently, there is no relation between the two resources other than the one (ConfigMap) being mounted in the other (Deployment). All you need in order to enable automatic rollouts in case of config map updates is to add a `deployment.kubernetes.io/triggered-by: configmap/alpine` annotation in the Deployment. This should be done by `kubectl set trigger` which is proposed in https://github.com/kubernetes/kubernetes/pull/31701 - for now we will add it via `kubectl annotate`:
+```
+$ kubectl annotate deploy/alpine deployment.kubernetes.io/triggered-by=configmap/alpine
+deployment "alpine" annotated
+```
+
+Now update the alpine config map and change the message that will be echoed:
+```
+$ kubectl edit cm/alpine
+configmap "alpine" edited
+```
+
+You should see a new rollout underway:
+```
+$ kubectl get po
+NAME                        READY     STATUS              RESTARTS   AGE
+alpine-1077172096-zg3h2     0/1       ContainerCreating   0          3s
+alpine-928338976-59vhm      1/1       Terminating         0          4m
+observer-3223417557-xf4aj   1/1       Running             0          6m
+```
+
+Inspect our configuration inside the new pod:
+```
+$ kubectl exec alpine-1077172096-zg3h2 -i -t -- cat /etc/my-key
+Updated!
+```
--- trigger.sh
+++ trigger.sh
@@ -1,30 +1,60 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 # Get the list of all deployments in the same namespace. If we ever move this into a controller
 # we should use a ConfigMapRefIndex to index the cache so we won't end up requeueing configs that
 # are copies.
-deployments=($(oc get deployment | awk '{print $1}'))
-# Remove NAME header
-unset deployments[0]
+deployments=$(oc get deployment -n $1 | awk '{print $1}')
 
-# Create a copy of configmap/$1. This should be mounted on every deployment that has a
-# configmap triggered-by annotation.
-oc get configmap $1 -o jsonpath='{.data}' > /tmp/$1.data
-cmName=$1-$(md5sum /tmp/$1.data | head -c8)
-# TODO: In case the same configmap already exists, we should reuse that
-oc create configmap $cmName --from-file /tmp/$1.data
 
+triggered=()
 for d in $deployments
 do
-	triggeredby="$(oc get deployment "${d}" -o go-template='{{index .metadata.annotations "deployment.kubernetes.io/triggered-by"}}')"
-	IFS='/' read resource name <<< "$triggeredby"
-	if [ "$1" == "$name" ]
+	if [ "$d" == "NAME" ]
 	then
-		echo "Triggering a new rollout for deployment \""${d}"\" based on "${resource}" \""${name}"\" update..."
-		# The controller will need to identify which volume has to be updated. It should
-		# go over all volumes in the deployment of $resource type and examine which one
-		# of those is the one it needs to update.
-		volumeName=$(oc get deployment/$d -o jsonpath='{.spec.template.spec.volumes[0].name}')
-		oc set volume deployment/$d --add --overwrite --name=$volumeName -t $resource --configmap-name=$cmName
+	    continue
 	fi
+
+	triggeredby="$(oc get deployment "${d}" -n $1 -o go-template='{{index .metadata.annotations "deployment.kubernetes.io/triggered-by"}}')"
+	IFS='/' read resource name <<< "$triggeredby"
+	if [ "$2" == "$name" ]
+	then
+		triggered+=($d)
+	fi
+done
+
+# If nothing is triggered then exit.
+if [ ${#triggered[@]} == 0 ]
+then
+    exit 0
+fi
+
+
+# Create a copy of configmap/$2. This should be mounted on every deployment that has a
+# configmap triggered-by annotation. For now we just copy the first key since there is
+# no clean way currently to copy a configmap to another configmap.
+# Watch out for the outcome of https://github.com/kubernetes/kubernetes/pull/32367
+# The deployment controller should add owner references to the copies of the configmap
+# in the replica sets it creates so that garbage collection can be facilitated.
+oc get configmap $2 -n $1 -o go-template='{{range $key, $value := .data}}{{$key}}={{$value}}{{end}}' > /tmp/$2.data
+cmName=$2-$(md5sum /tmp/$2.data | head -c8)
+echo "Creating a copy for configmap \""${2}"\": "${cmName}""
+IFS='=' read key value <<< $(cat /tmp/$2.data)
+oc create configmap $cmName -n $1 --from-literal=$key=$value
+
+
+for d in $triggered
+do
+	echo "Triggering a new rollout for deployment \""${d}"\" based on configmap \""${2}"\" update..."
+	# The controller will need to identify which volume has to be updated. It should
+	# be given a list of container names[0], look into those containers and identify which
+	# one is using the volume we are interested in.	
+	#
+	# [0] Maybe via another annotation? It seems we need a generic trigger object that would
+	# hold an object reference, a slice of container names, and maybe a way to disable the
+	# trigger, similar to DeploymentTriggerImageChangeParams in OpenShift.
+	# https://github.com/openshift/origin/blob/master/pkg/deploy/api/types.go#L378
+	volumeName=$(oc get deployment/$d -n $1 -o jsonpath='{.spec.template.spec.volumes[0].name}')
+	oc set volume deployment/$d -n $1 --add --overwrite --name=$volumeName -t configmap --configmap-name=$cmName
 done
